2616033fb948354b8270ea7ec006908e658b2909
Yanael Barbier <ybarbier@student.42.fr>
2016-03-07 15:55:38 +0100

    Get params with flags h and v

--- inc/ft_ping.h
+++ inc/ft_ping.h
@@ -6,15 +6,25 @@
 /*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/04 16:51:48 by ybarbier          #+#    #+#             */
-/*   Updated: 2016/03/04 16:53:20 by ybarbier         ###   ########.fr       */
+/*   Updated: 2016/03/07 15:40:19 by ybarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PING
-# define FT_PING
+#ifndef PING_H
+# define PING_H
 
 # include "libft.h"
 # include <stdio.h>
 # include <unistd.h>
+
+enum	e_flags
+{
+	FLAGS_V = 1 << 0,
+	FLAGS_H = 1 << 1
+};
+
+typedef struct	s_env {
+	int		flags;
+}				t_env;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,48 @@
 /*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/04 16:22:32 by ybarbier          #+#    #+#             */
-/*   Updated: 2016/03/04 17:05:25 by ybarbier         ###   ########.fr       */
+/*   Updated: 2016/03/07 15:54:58 by ybarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
-int		main(void)
+static void	pg_error_usage(void)
 {
+	ft_error_str_exit("Usage: ping [OPTIONS] HOST\n");
+}
+
+static void	pg_parse_flags_one_arg(t_env *env, char *str)
+{
+	if (ft_strchr(str, 'v') != NULL)
+		env->flags |= FLAGS_V;
+	if (ft_strchr(str, 'h') != NULL)
+		env->flags |= FLAGS_H;
+}
+
+static int	pg_parse_flags(t_env *env, int nb_args, char **args)
+{
+	int i;
+
+	i = 1;
+	while (i < nb_args && ft_strncmp("-", args[i], 1) == 0)
+	{
+		if (args[i][1] && ft_isdigit(args[i][1]) == 1)
+			break ;
+		pg_parse_flags_one_arg(env, args[i]);
+		++i;
+	}
+	return (i);
+}
+
+int			main(int argc, char **argv)
+{
+	t_env	env;
+	int		pos_args;
+
+	if (argc <= 1)
+		pg_error_usage();
+	env.flags = 0;
+	pos_args = pg_parse_flags(&env, argc, argv);
 	return (0);
 }
