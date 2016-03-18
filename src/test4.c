384090952a5dfae3627ef973a3bc9e4d1f5531d6
Yanaël <b.yanael@gmail.com>
2016-03-18 16:58:52 +0100

    ICMP request work on UNIX

--- src/test4.c
+++ src/test4.c
@@ -19,6 +19,7 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
 
 typedef unsigned char u4;
 typedef unsigned short u8;
@@ -48,6 +49,11 @@ struct s_icmp {
 
 int main(int argc, char **argv)
 {
+	if (argc != 3)
+	{
+		printf("Usage: %s <adresse ip dst> <adresse ip src>\n", argv[0]);
+		exit(1);
+	}
 	/*
 	   printf("u4: %lu\n", sizeof(u4));
 	   printf("u8: %lu\n", sizeof(u8));
@@ -64,32 +70,109 @@ int main(int argc, char **argv)
 	s_p = getprotobyname("icmp");
 
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = PF_INET;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_RAW;
+	hints.ai_protocol = IPPROTO_ICMP;
 
-	if (getaddrinfo(argv[1], argv[2], &hints, &res) == -1)
+	if (getaddrinfo(argv[1], NULL, &hints, &res) < 0)
 	{
-		printf("Error getaddrinfo");
+		perror("Error getaddrinfo");
 		exit(1);
 	}
 
 	// Socket
 	printf("t_ip: %lu\n", sizeof(t_ip));
-	char buf[400];
-	struct ip *ip = (struct ip*)buf;
-	struct icmphdr *icmp;
+//	char buf[400];
+//	struct ip *ip = (struct ip*)buf;
+//	struct icmphdr *icmp;
 //	ip->ip_v = 4;
 //	ip->ip_hl = 3;
 //	ip->ip_len = 200;
-/*
-	if ((s = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1)
+
+	printf("Family: %d\n", hints.ai_family);
+	printf("Socktype: %d\n", hints.ai_socktype);
+	printf("Protocol: %d\n", hints.ai_protocol);
+	printf("Family: %d\n", res->ai_family);
+	printf("Socktype: %d\n", res->ai_socktype);
+	printf("Protocol: %d\n", res->ai_protocol);
+
+	if ((s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0)
 	{
-		printf("Error getaddrinfo");
+		perror("socket() error");
 		exit(1);
 	}
-*/
+
+
 	printf("Socket: %d\n", s);
 
+	int hincl;
+
+	hincl = 1;
+	if (setsockopt(s, IPPROTO_IP, IP_HDRINCL, &hincl, sizeof(hincl)) < 0)
+	{
+		perror("setscokopt() error");
+		exit(1);
+	}
+
+	char buf[400];
+	// t_ip *ip = (t_ip *)buf;
+	struct ip *ip = (struct ip *)buf;
+	t_icmp *icmp = (t_icmp *)(ip + 1);
+
+	struct sockaddr_in dst;
+
+	bzero(buf, sizeof(buf));
+
+	ip->ip_v = 4;
+	ip->ip_tos = 0;
+	ip->ip_hl = sizeof(*ip) >> 2;
+	ip->ip_ttl = 200;
+	ip->ip_len = htons(sizeof(buf));
+	ip->ip_p = IPPROTO_ICMP;
+	ip->ip_sum = 0;
+
+	ip->ip_dst.s_addr = inet_addr(argv[1]);
+	ip->ip_src.s_addr = inet_addr(argv[2]);
+
+	dst.sin_addr = ip->ip_dst;
+	dst.sin_family = PF_INET;
+	printf("%s\n", inet_ntoa(ip->ip_dst));
+	printf("%s\n", inet_ntoa(ip->ip_src));
+
+	icmp->type = ICMP_ECHO;
+	icmp->code = 0;
+	// icmp->checksum = htons(~(ICMP_ECHO << 8));
+	icmp->checksum = 65527;
+	printf("Checksum: %d\n", icmp->type);
+	printf("Checksum: %d\n", ICMP_ECHO);
+	printf("Checksum: %d\n", ICMP_ECHO << 8);
+	printf("Checksum: %d\n", ~(ICMP_ECHO << 8));
+	printf("Checksum: %d\n", htons(~(ICMP_ECHO << 8)));
+
+	printf("Checksum: %u\n", ntohs(icmp->checksum));
+	printf("Checksum: %u\n", ntohs(~icmp->checksum));
+	printf("Checksum: %u\n", (ntohs((~icmp->checksum))) >> 8);
+
+	printf("Test: %u\n", ntohs(1));
+
+	printf("Buf: %lu\n", sizeof(buf));
+	// for (int i = 0; i < sizeof(buf); i++)
+	// {
+	// 	printf("%01X:", buf[i]);
+	// }
+	// exit(1);
+
+	int nb_send;
+
+	if ((nb_send = sendto(s, buf, sizeof(buf), 0, res->ai_addr, res->ai_addrlen)) < 0)
+	{
+
+	// if ((nb_send = sendto(s, buf, sizeof(buf), 0, (struct sockaddr *)&dst, sizeof(dst))) < 0)
+	// {
+		perror("Error sendto");
+		exit(1);
+	}
 
 	return (0);
 }
+
