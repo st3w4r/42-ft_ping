8485f66450ece4a12a3b973342c6538be844be6a
Yanael Barbier <ybarbier@student.42.fr>
2016-04-04 14:04:06 +0200

    Add timer request

--- src/pg_loop.c
+++ src/pg_loop.c
@@ -6,19 +6,20 @@
 /*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/31 15:52:17 by ybarbier          #+#    #+#             */
-/*   Updated: 2016/03/31 18:44:28 by ybarbier         ###   ########.fr       */
+/*   Updated: 2016/04/04 14:03:16 by ybarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
-void	pg_display_response(t_env *env, int bytes_receive, int seq)
+void	pg_display_response(t_env *env, int bytes_receive, int seq,
+		double duration)
 {
 	char ip[INET_ADDRSTRLEN];
 
 	inet_ntop(env->res->ai_family, &(env->ip->ip_src.s_addr), ip, sizeof(ip));
-	printf("%d bytes from %s: icmp_seq=%d ttl=%d time= ms\n",
-	bytes_receive, ip, seq, env->ip->ip_ttl);
+	printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.3lf ms\n",
+	bytes_receive, ip, seq, env->ip->ip_ttl, duration);
 }
 
 static void	pg_timer(int interval)
@@ -46,27 +47,43 @@ void	pg_loop(t_env *env)
 	int			packets_send;
 	int			packets_receive;
 	unsigned short	seq;
+	struct timeval	tv_start;
+	struct timeval	tv_end;
+	double	duration;
 
 	packets_send = 0;
 	packets_receive = 0;
 	seq = 0;
 	while (packets_send < 10)
 	{
-		pg_configure_send(env, 6921, seq);
+//			printf("tv_usec: %ld\n", tv_current.tv_usec / 1000);
+
+		pg_configure_send(env, 42, seq);
 
 		if ((nb_send = sendto(env->s, env->buf, sizeof(env->buf), 0,
 			env->res->ai_addr, env->res->ai_addrlen)) < 0)
 			ft_error_str_exit("Error sendto\n");
 		if (nb_send >= 0)
+		{
+			gettimeofday(&tv_start, NULL);
+			//printf("tv_usec: %ld\n", tv_start.tv_usec / 1000);
+			//printf("tv_usec: %ld ", tv_duration.tv_usec);
 			packets_send++;
+		}
 
 		pg_configure_receive(env);
 
 		nb_receive = recvmsg(env->s, &(env->msg), 0);
 		if (nb_receive >= 0)
+		{
+			gettimeofday(&tv_end, NULL);
+//			tv_duration.tv_sec -= tv_start.tv_sec;
+//			tv_duration.tv_usec -= tv_start.tv_usec;
+			duration = (((double)tv_end.tv_sec * 1000000.0 + tv_end.tv_usec) - \
+			((double)tv_start.tv_sec * 1000000.0 + tv_start.tv_usec)) / 1000;
 			packets_receive++;
-
-		pg_display_response(env, nb_receive, seq);
+		}
+		pg_display_response(env, nb_receive, seq, duration);
 		seq++;
 		printf("Recieve: %d\n", nb_receive);
 		pg_timer(1);
